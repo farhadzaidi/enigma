525e2fc9e22ca5ccfc7537cc1c300cbd5b989348
Farhad Zaidi <zaidi.farhad03@gmail.com>
2025-10-05 14:21:58 -0400

    fix start pos fen

--- include/types.hpp
+++ include/types.hpp
@@ -167,7 +167,7 @@ constexpr uint16_t NULL_MOVE      = 0;
 // --- FEN Strings ---
 
 constexpr const char* START_POS_FEN = 
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1";
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 constexpr const char* KIWIPETE_FEN = 
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
 constexpr const char* POSITION_3_FEN = // Castling, en passant, and promotions
